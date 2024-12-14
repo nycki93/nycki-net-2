b593eb1968e88395d6e888a891dc2264a4db0b4f
Nicholas Lamicela <nupanick@gmail.com>
2024-12-13 19:32:29 -0800

    update author info

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,7 +3,7 @@ export const url = "https://nycki.net";
 export const language = "en";
 export const description = "nycki.net";
 export const author = {
-	name: "Your Name Here",
-	email: "youremailaddress@example.com",
-	url: "https://example.com/about-me/"
+	name: "nycki",
+	email: "nupanick@gmail.com",
+	url: "https://nycki.net"
 };
